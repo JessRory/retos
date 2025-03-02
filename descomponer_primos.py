371dcf589d156220ba6453a0852fdd3d6786ce8d
JessRory <josuriera@live.com>
2025-03-02 17:04:32 +0100

    Añadida la funcionalidad de reconocer los números primos

--- descomponer_primos.py
+++ descomponer_primos.py
@@ -1,48 +1,67 @@
 """append() agrega un elemento tal cual a la lista (lo que crea listas anidadas si el elemento es otra lista).
 += extiende la lista actual con los elementos de otra lista (lo que queremos en este caso)."""
 
-def des_num_prim(numero):
+
+def numero_primo(num):
+    
+    for n in range(2, num):
+        
+        if num % n == 0:
+            
+            return False
+        
+    print(f'El número {num} es primo')
+    
+    return True
+
+def des_num_prim(numero, llamada=True):
     
     lista_primos = []
 
+    # Solo verificamos si es primo en la primera llamada
+    if llamada and numero_primo(numero):
+        print('Por lo tanto, sólo es divisible entre uno y sí mismo.')
+        
+        return [numero, 1]
+
+    # Factorización en números primos
+    
     if numero == 1:
         
-        lista_primos.append(numero)
-
+        return [1]
+    
     elif numero % 2 == 0:
         
-        # Usamos // para obtener un número entero:
-        valor = numero // 2  
+        valor = numero // 2
         
         lista_primos.append(2)
         
-         # Concatenamos el resultado de la recursión:
-        lista_primos += des_num_prim(valor)  # Concatenamos el resultado de la recursión
-        
+        lista_primos += des_num_prim(valor, False)
+
     elif numero % 3 == 0:
         
         valor = numero // 3
         
         lista_primos.append(3)
         
-        lista_primos += des_num_prim(valor)
-        
+        lista_primos += des_num_prim(valor, False)
+
     elif numero % 5 == 0:
         
         valor = numero // 5
         
         lista_primos.append(5)
         
-        lista_primos += des_num_prim(valor)
-        
-    else:
-        
-        print('Comando inválido')
+        lista_primos += des_num_prim(valor, False)
+
+    #else:
+        #print('Comando inválido')
 
     return lista_primos
 
 # Prueba
-numeros_primos = des_num_prim(70)
-print(numeros_primos)  # Salida esperada: [2, 2, 2]
+numeros_primos = des_num_prim(444)
+print(numeros_primos)
+
 
 
